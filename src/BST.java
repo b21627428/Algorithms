4c0118521314c676ce5256d3bacf06a3b58830a9
Muhammed Said Kaya <saidkaya1239@gmail.com>
2021-03-02 16:40:14 +0300

    binary search tree deletion function added.

--- src/BST.java
+++ src/BST.java
@@ -49,7 +49,43 @@ public class BST<K extends Comparable<K>,V>{
         inOrderValues(root,values);
         return values;
     }
+    public void delete(K key){
+        root = delete(root,key);
+    }
 
+    //HIBBARD DELETION
+    private Node delete(Node x, K key) {
+        if(x==null) return null;
+        int cmp= key.compareTo(x.key);
+        if(cmp<0) x.left=delete(x.left,key);
+        else if(cmp>0) x.right=delete(x.right,key);
+        else{
+            if(x.right==null) return x.left;
+            else{
+                Node t=x;
+                x=minNode(t.right);
+                x.right = deleteMin(t.right);
+                x.left = t.left;
+            }
+        }
+        return x;
+    }
+
+
+    private void deleteMin(){
+        root=deleteMin(root);
+    }
+    private Node deleteMin(Node x) {
+        if(x==null) return null;
+        else if(x.left ==null) return x.right;
+        else x.left=deleteMin(x.left);
+        return x;
+    }
+    private Node minNode(Node x){
+        if(x==null) return null;
+        else if(x.left==null) return x;
+        else return minNode(x.left);
+    }
     private void inOrder(Node x, Queue<K> keys) {
         if(x==null) return;
         inOrder(x.left,keys);
--- src/Main.java
+++ src/Main.java
@@ -9,45 +9,45 @@ public class Main {
         Person person3 = new Person(7L, "Furkan", "Kaya");
         Person[] people = {person1, person2,person3};
 
-//        Sort.selectionSort(people);
-//        System.out.println(Arrays.toString(people));
-//        Sort.shuffle(people);
-//
-//        Sort.insertionSort(people);
-//        System.out.println(Arrays.toString(people));
-//        Sort.shuffle(people);
-//
-//        Sort.shellSort(people);
-//        System.out.println(Arrays.toString(people));
-//        Sort.shuffle(people);
-//
-//        Sort.mergeSort(people);
-//        System.out.println(Arrays.toString(people));
-//        Sort.shuffle(people);
-//
-//        Sort.mergeSort(people,false);
-//        System.out.println(Arrays.toString(people));
-//        Sort.shuffle(people);
-//
-//        Sort.bottomUpMergeSort(people);
-//        System.out.println(Arrays.toString(people));
-//        Sort.shuffle(people);
-//
-//        Sort.quickSort(people,false);
-//        System.out.println(Arrays.toString(people));
-//        Sort.shuffle(people);
-//
-//        Sort.quickSort(people,true);
-//        System.out.println(Arrays.toString(people));
-//        Sort.shuffle(people);
-//
-//        MaxHeap<Person> personHeap = new MaxHeap<>(Arrays.asList(people));
-//        personHeap.sort();
-//        System.out.println(personHeap);
-//
-//        Sort.quickSort(people,false);
-//        int resultIndex = Search.binarySearch(people, person3);
-//        System.out.println(resultIndex);
+        Sort.selectionSort(people);
+        System.out.println(Arrays.toString(people));
+        Sort.shuffle(people);
+
+        Sort.insertionSort(people);
+        System.out.println(Arrays.toString(people));
+        Sort.shuffle(people);
+
+        Sort.shellSort(people);
+        System.out.println(Arrays.toString(people));
+        Sort.shuffle(people);
+
+        Sort.mergeSort(people);
+        System.out.println(Arrays.toString(people));
+        Sort.shuffle(people);
+
+        Sort.mergeSort(people,false);
+        System.out.println(Arrays.toString(people));
+        Sort.shuffle(people);
+
+        Sort.bottomUpMergeSort(people);
+        System.out.println(Arrays.toString(people));
+        Sort.shuffle(people);
+
+        Sort.quickSort(people,false);
+        System.out.println(Arrays.toString(people));
+        Sort.shuffle(people);
+
+        Sort.quickSort(people,true);
+        System.out.println(Arrays.toString(people));
+        Sort.shuffle(people);
+
+        MaxHeap<Person> personHeap = new MaxHeap<>(Arrays.asList(people));
+        personHeap.sort();
+        System.out.println(personHeap);
+
+        Sort.quickSort(people,false);
+        int resultIndex = Search.binarySearch(people, person3);
+        System.out.println(resultIndex);
 
         BST<Long,Person> bst = new BST<>();
         bst.put(person1.getId(),person1);
@@ -55,5 +55,7 @@ public class Main {
         bst.put(person3.getId(),person3);
         System.out.println(bst.get(1L));
         System.out.println(bst.keys());
+        bst.delete(5L);
+        System.out.println(bst.values());
     }
 }
