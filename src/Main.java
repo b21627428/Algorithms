5263132efe538a6c2f7ba72b0d07e157c94339b6
Muhammed Said Kaya <saidkaya1239@gmail.com>
2021-03-02 15:21:18 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -9,37 +9,37 @@ public class Main {
         Person person3 = new Person(7L, "Furkan", "Kaya");
         Person[] people = {person1, person2,person3};
 
-//        Sort.selectionSort(people);
-//        System.out.println(Arrays.toString(people));
-//        Sort.shuffle(people);
-//
-//        Sort.insertionSort(people);
-//        System.out.println(Arrays.toString(people));
-//        Sort.shuffle(people);
-//
-//        Sort.shellSort(people);
-//        System.out.println(Arrays.toString(people));
-//        Sort.shuffle(people);
-//
-//        Sort.mergeSort(people);
-//        System.out.println(Arrays.toString(people));
-//        Sort.shuffle(people);
-//
-//        Sort.mergeSort(people,false);
-//        System.out.println(Arrays.toString(people));
-//        Sort.shuffle(people);
-//
-//        Sort.bottomUpMergeSort(people);
-//        System.out.println(Arrays.toString(people));
-//        Sort.shuffle(people);
-//
-//        Sort.quickSort(people,false);
-//        System.out.println(Arrays.toString(people));
-//        Sort.shuffle(people);
-//
-//        Sort.quickSort(people,true);
-//        System.out.println(Arrays.toString(people));
-//        Sort.shuffle(people);
+        Sort.selectionSort(people);
+        System.out.println(Arrays.toString(people));
+        Sort.shuffle(people);
+
+        Sort.insertionSort(people);
+        System.out.println(Arrays.toString(people));
+        Sort.shuffle(people);   
+
+        Sort.shellSort(people);
+        System.out.println(Arrays.toString(people));
+        Sort.shuffle(people);
+
+        Sort.mergeSort(people);
+        System.out.println(Arrays.toString(people));
+        Sort.shuffle(people);
+
+        Sort.mergeSort(people,false);
+        System.out.println(Arrays.toString(people));
+        Sort.shuffle(people);
+
+        Sort.bottomUpMergeSort(people);
+        System.out.println(Arrays.toString(people));
+        Sort.shuffle(people);
+
+        Sort.quickSort(people,false);
+        System.out.println(Arrays.toString(people));
+        Sort.shuffle(people);
+
+        Sort.quickSort(people,true);
+        System.out.println(Arrays.toString(people));
+        Sort.shuffle(people);
 
         MaxHeap<Person> personHeap = new MaxHeap<>(Arrays.asList(people));
         personHeap.sort();
